8112a3df33e0e0c722f895c013bef18f3b62ad76
Vincent de Bruijn <bruijnv@gmail.com>
2019-01-04 15:21:04 -0800

    Update loader

--- Assets/Scripts/VRGraph/VRGraph/Loader.cs
+++ Assets/Scripts/VRGraph/VRGraph/Loader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GraphVisualisation;
+using VRGraph.Json;
 
 namespace VRGraph {
 	public class Loader : MonoBehaviour {
@@ -10,19 +11,41 @@ namespace VRGraph {
 
 		Game<String> game;
 
-		// Use this for initialization
 		void Start () {
-			TextAsset txt = Resources.Load(resource) as TextAsset;
-			string json = txt.text;
 			Graph g = new Graph();
-			JsonUtility.FromJsonOverwrite(json, g);
-			Debug.Log("Finished loading json. Nodes: " + g.nodes.length + ", Edges: " + g.edges.length);
-			this.game = new Game<String>();
-			this.game.GenerateNodes(g);
+			parseJson(g);
+			Debug.Log("Finished parsing json. Nodes: " + g.nodes.length + ", Edges: " + g.edges.length);
+			createGame(g);
 		}
 
 		void Update() {
 			this.game.Update();
+		}
+		
+
+		private void parseJson(Graph g) {
+			TextAsset txt = Resources.Load(resource) as TextAsset;
+			string json = txt.text;
+			JsonUtility.FromJsonOverwrite(json, g);
+		}
+
+		private void createGame(Graph g) {
+			Dictionary<int, string> nodes = new Dictionary<int, string>();
+			Dictionary<string, int> ids = new Dictionary<string, int>();
+			List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
+
+			foreach(Node n in g.nodes) {
+				int id = nodes.Count;
+				nodes.Add(id, n.label);
+				ids.Add(n.id, id);
+			}
+
+			foreach(Edge e in g.edges) {
+				Tuple<int, int> edge = new Tuple<int, int>(ids[e.from], ids[e.to]);
+				edges.Add(edge);
+			}
+
+			this.game = new Game<String>(edges, nodes);
 		}
 	}
 }
