8d9d47fcbdb007ca706cf06d274d2f352fdcba3d
Vincent de Bruijn <bruijnv@gmail.com>
2019-01-04 10:08:39 -0800

    Initial unity project

---
+++ Assets/Scripts/3d-graph/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+	public String resource = "test.json";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
