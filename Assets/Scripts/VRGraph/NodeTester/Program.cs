c4c8671ffb7bfd15f172a2306dfba8bf930063bd
Michiel <michiel.doorn@gmail.com>
2019-01-18 11:04:09 +0100

    fix git try 2

---
+++ Assets/Scripts/VRGraph/NodeTester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using VRGraph.GraphVisualisation;
+
+namespace NodeTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tuple<int, int>> edges = new List<Tuple<int, int>>();
+            Dictionary<int, int> nodes = new Dictionary<int, int>();
+            edges.Add(new Tuple<int, int>(0, 1));
+            edges.Add(new Tuple<int, int>(2, 1));
+            edges.Add(new Tuple<int, int>(0, 3));
+            nodes.Add(0, 0);
+            nodes.Add(1, 1);
+            nodes.Add(2, 2);
+            nodes.Add(3, 3);
+
+            Game<int> game = new Game<int>(edges, nodes);
+
+
+            float numberOfUpdates = 10 / Game<string>.DeltaTime;
+            for(int x = 0; x < numberOfUpdates; x++)
+            {
+                game.Update();
+            }
+
+        }
+    }
+}
