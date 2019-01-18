9b5f3ab699ffe38ccc9ab1752ad23f14dd331fd1
Vincent de Bruijn <bruijnv@gmail.com>
2019-01-18 09:38:06 -0800

    Fix loader

--- Assets/Scripts/VRGraph/VRGraph/Loader.cs
+++ Assets/Scripts/VRGraph/VRGraph/Loader.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GraphVisualisation;
 using VRGraph.Json;
+using VRGraph.GraphVisualisation;
 
 namespace VRGraph {
 	public class Loader : MonoBehaviour {
