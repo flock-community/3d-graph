6295f663e744317982a8bd60edb9adf4693b2c46
Vincent de Bruijn <bruijnv@gmail.com>
2019-02-15 02:34:44 -0800

    Show labels above nodes when mouse over or click

--- Assets/Scripts/VRGraph/VRGraph/LabelController.cs
+++ Assets/Scripts/VRGraph/VRGraph/LabelController.cs
@@ -5,6 +5,7 @@ using VRGraph.Json;
 using VRGraph.GraphVisualisation;
 using VRGraph.Utilities;
 using NodeTester;
+using TMPro;
 
 namespace VRGraph {
 	public class LabelController : MonoBehaviour {
@@ -12,6 +13,14 @@ namespace VRGraph {
         public string label;
         public bool labelShowsOnClick;
         public float labelShowsAfterTime = Mathf.Infinity;
+
+        private TextMeshPro textMeshPro;
+
+        void Start() {
+            textMeshPro = gameObject.GetComponentInChildren<TextMeshPro>(true);
+            Debug.Log(textMeshPro);
+            textMeshPro.text = label;
+        }
 
         void OnMouseDown()
         {
@@ -21,20 +30,32 @@ namespace VRGraph {
         }
 
         void OnMouseEnter() {
+            StopAllCoroutines();
             StartCoroutine(waitToShowLabel());
         }
 
         void OnMouseExit() {
             StopAllCoroutines();
+            StartCoroutine(waitToHideLabel());
         }
 
         void showLabel() {
             Debug.Log("Label: "+ label);
+            textMeshPro.gameObject.SetActive(true);
+        }
+
+        void hideLabel() {
+            textMeshPro.gameObject.SetActive(false);
         }
 
         IEnumerator waitToShowLabel() {
             yield return new WaitForSeconds(labelShowsAfterTime);
             showLabel();
+        }
+
+        IEnumerator waitToHideLabel() {
+            yield return new WaitForSeconds(labelShowsAfterTime);
+            hideLabel();
         }
     }
 }
